069c12b8bb6429a22e5aaf8b0ec271253f422303
DarchanKaen <darchankaen@gmail.com>
2023-06-23 18:51:37 +0300

    v002. Add logic to db class

--- src/EIVT_db_worker.py
+++ src/EIVT_db_worker.py
@@ -1,2 +1,25 @@
+import sqlite3
+
+
 class DBWorker:
-    pass
+    def __init__(self):
+        self.db_name = "eivt_db"
+        self.connection = None
+        self.cursor = None
+
+    def connect(self):
+        self.connection = sqlite3.connect(self.db_name)
+        self.cursor = self.connection.cursor()
+
+    def disconnect(self):
+        self.connection.close()
+
+    def execute(self, querry):
+        self.cursor.execute(querry)
+
+    def createTable(self):
+        create_table_query = "CREATE TABLE eivt (id INTEGER PRIMARY_KEY, form_I TEXT, form_II TEXT, form_III TEXT)"
+        self.cursor.execute(create_table_query)
+
+
+
